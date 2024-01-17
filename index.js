ed7c27d951a70ba7e21426211baf286a2c80634f
Stephanie Bertram <steph.a.bertram@gmail.com>
2024-01-17 12:31:11 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+    let newArray = []
+    for (let i = 0; i < names.length; i++) {
+        newArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+
+  return newArray;
+}
+
+function countDown(number) {
+    while (number >= 0); {
+        console.log(number--);
+    }
+}
